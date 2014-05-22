336785f06ada4110b69d7c000e35415be1aa9f15
Ahmad Faisal Hammad <afhammad@gmail.com>
2014-05-22 11:14:38 +0100

    add supoorts_options? method to FieldType

--- lib/customly/field_type.rb
+++ lib/customly/field_type.rb
@@ -14,6 +14,10 @@ module Customly
       @html_options = {}
     end
 
+    def supports_options?
+      [:select].include? self.input_type
+    end
+
     def self.find_by_key(k)
       Customly.configuration.field_types[k.to_sym]
     end
